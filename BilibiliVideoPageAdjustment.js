a88baf09a974656a43a379aaba7017c30d52677f
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2022-01-21 11:06:49 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name              BiliBili播放页调整（自用）
+// @name              BiliBili播放页调整
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
 // @version           0.2
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
