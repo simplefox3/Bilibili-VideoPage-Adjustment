0df278ebcc74250c08faaef80cdf7208685d1ccd
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2022-01-23 00:07:34 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              BiliBili播放页调整
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.2.4
+// @version           0.2.4.1
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -103,9 +103,9 @@ $(function() {
                 });
             }
             if (util.exist('#bilibili-player')) {
-                const playerDataScreen = $('#bilibili-player .bpx-player-container').attr('data-screen');
                 let mutationObserver = new MutationObserver(function(mutations) {
                     mutations.forEach(function(mutation) {
+                        const playerDataScreen = $('#bilibili-player .bpx-player-container').attr('data-screen');
                         if (playerDataScreen === 'normal') {
                             util.setValue('current_screen_mod', 'normal')
                         }
